2841e05752b75d77b40a335a575aac5f48e88002
Alexander Peter <Alexander Peter>
2016-10-19 00:53:12 +0300

    Added Entity

---
+++ PracticeTask2016/PracticeTask2016/Entity/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask2016.Entity
+{
+    public class Adress
+    {
+        public string street { get; set; }
+        public string house { get; set; }
+        public string apartment { get; set; }
+    }
+}
---
+++ PracticeTask2016/PracticeTask2016/Entity/Employees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask2016.Entity
+{
+    public class Employees
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+        public DateTime birthday { get; set; }
+        public string phoneNumber { get; set; }
+        public Adress adress { get; set; }
+    }
+}
